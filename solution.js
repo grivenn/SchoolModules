6b1a5058e6d0983e7e32667e471c51baef4423c9
DESKTOP-NN816ET\ogriv <ogriven@gmail.com>
2023-04-28 10:40:32 +0300

    Added solution.js

---
+++ solution.js
@@ -0,0 +1,70 @@
+
+
+
+export function getObserverCallback(updateBreadcrumbs, currentHeader = null) {
+  // решение
+  if (currentHeader == null) return;
+
+  let BreadcrumbsArr = [];
+  let allHeadersList = {};
+  allHeadersList = parse(allHeadersList);
+
+  let parentId = currentHeader;
+  let headerLevel = document.getElementById(currentHeader).dataset.header;
+
+  while (parentId > 0) {
+    let id = parentId;
+    let nextHd = allHeadersList[headerLevel][id];
+    parentId = nextHd.parentId;
+    headerLevel = nextHd.parentLevel;
+    BreadcrumbsArr.push(id);
+  }
+  BreadcrumbsArr.push(0);
+
+  BreadcrumbsArr.reverse();
+  updateBreadcrumbs(BreadcrumbsArr);
+}
+
+
+// парсинг div[data-header]
+function parse(allHeadersList) {
+    let parentId = 0;
+    let parentLevel = 1;
+    let stec = [0];
+    let oldElID = 0;
+    let oldElLevel = 0;
+  
+    let newElID = 0;
+    let newElLevel = 0;
+  
+    document.querySelectorAll("div[data-header]").forEach((el) => {
+      oldElID = newElID;
+      oldElLevel = newElLevel;
+  
+      newElID = Number(el.id);
+      newElLevel = Number(el.dataset.header);
+  
+      // снижение
+      if (newElLevel - oldElLevel < 0) {
+        let steps = oldElLevel - newElLevel;
+        [parentId, parentLevel] = stec.splice(stec.length - steps, steps).shift();
+      }
+      // подъем
+      else if (newElLevel - oldElLevel > 0) {
+        stec.push([parentId, parentLevel]);
+        parentId = oldElID;
+        parentLevel = oldElLevel;
+      }
+  
+      if (!allHeadersList[newElLevel]) {
+        allHeadersList[newElLevel] = {};
+      }
+  
+      allHeadersList[newElLevel][newElID] = {
+        parentId: parentId,
+        parentLevel: parentLevel,
+      };
+    });
+  
+    return allHeadersList;
+  }
