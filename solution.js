de3e0f79bf4c5bf376ecc177e5880dc52804a77b
DESKTOP-NN816ET\ogriv <ogriven@gmail.com>
2023-04-28 16:31:39 +0300

    solution.js changed

--- solution.js
+++ solution.js
@@ -1,13 +1,19 @@
+allHeadersList = {};
 
+export function getObserverCallback(updateBreadcrumbs) {
+  return getHeadersIdList;
+}
 
-
-export function getObserverCallback(updateBreadcrumbs, currentHeader = null) {
-  // решение
-  if (currentHeader == null) return;
+function getHeadersIdList() {
+  const { target } = this;
+  currentHeader = target.id;
+  if (!currentHeader) return;
 
   let BreadcrumbsArr = [];
-  let allHeadersList = {};
-  allHeadersList = parse(allHeadersList);
+
+  if (Object.keys(allHeadersList).length == 0) {
+    allHeadersList = parse(allHeadersList);
+  }
 
   let parentId = currentHeader;
   let headerLevel = document.getElementById(currentHeader).dataset.header;
@@ -26,49 +32,44 @@ export function getObserverCallback(updateBreadcrumbs, currentHeader = null) {
 }
 
 
-// =====================================================================================
 // парсинг div[data-header]
-
-// Думаю, что по уму, результат парсинга надо где-то кэшировать, что бы не перелопачивать его каждый раз.
-
 function parse(allHeadersList) {
-    let parentId = 0;
-    let parentLevel = 1;
-    let stec = [0];
-    let oldElID = 0;
-    let oldElLevel = 0;
-  
-    let newElID = 0;
-    let newElLevel = 0;
-  
-    document.querySelectorAll("div[data-header]").forEach((el) => {
-      oldElID = newElID;
-      oldElLevel = newElLevel;
-  
-      newElID = Number(el.id);
-      newElLevel = Number(el.dataset.header);
-  
+  let parentId = 0;
+  let parentLevel = 1;
+  let stec = [0];
+  let oldElID = 0;
+  let oldElLevel = 0;
+
+  let newElID = 0;
+  let newElLevel = 0;
+
+  document.querySelectorAll("div[data-header]").forEach((el) => {
+    oldElID = newElID;
+    oldElLevel = newElLevel;
+
+    newElID = Number(el.id);
+    newElLevel = Number(el.dataset.header);
+
+    if (newElLevel - oldElLevel < 0) {
       // снижение
-      if (newElLevel - oldElLevel < 0) {
-        let steps = oldElLevel - newElLevel;
-        [parentId, parentLevel] = stec.splice(stec.length - steps, steps).shift();
-      }
+      let steps = oldElLevel - newElLevel;
+      [parentId, parentLevel] = stec.splice(stec.length - steps, steps).shift();
+    } else if (newElLevel - oldElLevel > 0) {
       // подъем
-      else if (newElLevel - oldElLevel > 0) {
-        stec.push([parentId, parentLevel]);
-        parentId = oldElID;
-        parentLevel = oldElLevel;
-      }
-  
-      if (!allHeadersList[newElLevel]) {
-        allHeadersList[newElLevel] = {};
-      }
-  
-      allHeadersList[newElLevel][newElID] = {
-        parentId: parentId,
-        parentLevel: parentLevel,
-      };
-    });
-  
-    return allHeadersList;
-  }
+      stec.push([parentId, parentLevel]);
+      parentId = oldElID;
+      parentLevel = oldElLevel;
+    }
+
+    if (!allHeadersList[newElLevel]) {
+      allHeadersList[newElLevel] = {};
+    }
+
+    allHeadersList[newElLevel][newElID] = {
+      parentId: parentId,
+      parentLevel: parentLevel,
+    };
+  });
+
+  return allHeadersList;
+}
